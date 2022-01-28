54f59acf5261370e5981871f5b9e8eddd2fe2330
A.Ukolov <sanjrostov@mail.ru>
2022-01-28 21:23:26 +0300

    poio

---
+++ Login form/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Login form
+//
+//  Created by Александр Уколов on 22.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // MARK: -
+    private let user = "User"
+    private let password = "Password"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: - background
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "bg_image")
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        loginButton.layer.cornerRadius = 10
+        
+    }
+    
+    // MARK: - help user
+    @IBAction func forgotUserName() {
+        let alert = UIAlertController(title: "Attention", message: "Your name is \(user)", preferredStyle: .alert)
+        let okBtn = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okBtn)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - help passwort
+    @IBAction func forgotUserPassport() {
+        let alert = UIAlertController(title: "Attention", message: "Your password is \(password)", preferredStyle: .alert)
+        let okBtn = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okBtn)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: -
+
+        
+    }
+
+
--- Login form/UserViewController.swift
+++ Login form/UserViewController.swift
@@ -17,18 +17,11 @@ class UsersViewController: UIViewController {
         backgroundImage.image = UIImage(named: "IMG_3537")
         self.view.insertSubview(backgroundImage, at: 0)
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func LogOut() {
+        dismiss(animated: true)
     }
-    */
-
+    
 }
