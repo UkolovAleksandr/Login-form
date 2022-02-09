0edc147f13ba530430b4716257266f20588f63ed
A.Ukolov <sanjrostov@mail.ru>
2022-02-09 19:50:12 +0300

    rtr

--- Login form/LoginViewController.swift
+++ Login form/LoginViewController.swift
@@ -7,21 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+
     @IBOutlet weak var loginButton: UIButton!
-    
-    // MARK: -
+
     private let user = "User"
     private let password = "Password"
     
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let WelcomeVC = segue.destination as? WelcomeViewController else { return }
+        WelcomeVC.user = user
+    }
+    
+
+    @IBAction func logInPressed() {
+        if userNameTextField.text != user || passwordTextField.text != password {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+            )
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-   
-        // MARK: - background
+        
+
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "bg_image")
         self.view.insertSubview(backgroundImage, at: 0)
@@ -29,7 +46,6 @@ class ViewController: UIViewController {
         loginButton.layer.cornerRadius = 10
     }
     
-    // MARK: - help user
     @IBAction func forgotUserName() {
         let alert = UIAlertController(title: "Attention", message: "Your name is \(user)", preferredStyle: .alert)
         let okBtn = UIAlertAction(title: "Ok", style: .default, handler: nil)
@@ -38,14 +54,48 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    // MARK: - help passwort
-    @IBAction func forgotUserPassport() {
+    
+    @IBAction func forgotUserPassword() {
         let alert = UIAlertController(title: "Attention", message: "Your password is \(password)", preferredStyle: .alert)
         let okBtn = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(okBtn)
         
         present(alert, animated: true, completion: nil)
     }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
 }
 
+extension LoginViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInPressed()
+            performSegue(withIdentifier: "showWelcomeVC", sender: nil)
+        }
+        return true
+    }
+}
 
